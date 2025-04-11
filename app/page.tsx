66c707e6f4d8c7a92829e93d92e1db7ddbd3b415
coispartan <hignacio_irri@hotmail.com>
2025-04-11 11:54:20 -0400

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-white font-black">
+    <h1 className="text-3xl text-primary-500 font-black">
       Welcome to the world of Next.JS
     </h1>
   );
