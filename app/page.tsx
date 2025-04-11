40de3e650533dc635452576dade16ae39d74471b
coispartan <hignacio_irri@hotmail.com>
2025-04-11 17:40:04 -0400

    Final version of tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-const Home = () => {
-  return (
-    <h1 className="text-3xl text-primary-500 font-black">
-      Welcome to the world of Next.JS
-    </h1>
-  );
-};
+const Home = () => <h1 className="h1-bold">Welcome to the world of Next.js</h1>;
 
 export default Home;
