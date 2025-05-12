77e0c1f84243ebbb7769c829892f168afac5c860
coispartan <hignacio_irri@hotmail.com>
2025-05-12 10:57:25 -0400

    JsMastery LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,19 @@
 import { ReactNode } from "react";
 
 import Navbar from "@/components/navigation/navbar";
-import LeftSiderBar from "@/components/navigation/navbar/LeftSiderBar";
+import LeftSiderBar from "@/components/navigation/LeftSiderBar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      <LeftSiderBar />
-      {children}
+      <div className="flex">
+        <LeftSiderBar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
     </main>
   );
 };
--- components/navigation/navbar/AuthStatus.tsx
+++ components/navigation/navbar/AuthStatus.tsx
@@ -19,7 +19,13 @@ export default async function AuthActions() {
           type="submit"
           className="flex items-center justify-start gap-4 bg-transparent p-4"
         >
-          <Image src="/icons/Vector.png" width={24} height={24} alt="SignOut" />
+          <Image
+            src="/icons/Vector.png"
+            width={24}
+            height={24}
+            alt="SignOut"
+            className="invert-colors"
+          />
           <p className="text-dark300_light900 base-bold">Logout</p>
         </Button>
       </form>
@@ -28,16 +34,36 @@ export default async function AuthActions() {
 
   return (
     <>
-      <Link href={ROUTES.SIGN_IN}>
-        <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-          <span className="primary-text-gradient">Log In</span>
-        </Button>
-      </Link>
-      <Link href={ROUTES.SIGN_UP}>
-        <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-          Sign up
-        </Button>
-      </Link>
+      <Button
+        className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+        asChild
+      >
+        <Link href={ROUTES.SIGN_IN}>
+          <Image
+            src="/icons/account.svg"
+            alt="Account"
+            width={20}
+            height={20}
+            className="invert-colors lg:hidden"
+          />
+          <span className="primary-text-gradient max-lg:hidden">Log In</span>
+        </Link>
+      </Button>
+      <Button
+        className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none"
+        asChild
+      >
+        <Link href={ROUTES.SIGN_UP}>
+          <Image
+            src="/icons/sign-up.svg"
+            alt="Account"
+            width={20}
+            height={20}
+            className="invert-colors lg:hidden"
+          />
+          <span className="max-lg:hidden">Sign up</span>
+        </Link>
+      </Button>
     </>
   );
 }
