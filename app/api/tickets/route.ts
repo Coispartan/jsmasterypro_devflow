2cd39a0c935a2461406fd2169e15891d88d578f0
coispartan <hignacio_irri@hotmail.com>
2025-05-23 11:53:06 -0400

    Route Handlers Implementation

---
+++ app/api/tickets/route.ts
@@ -0,0 +1,14 @@
+import tickets from "@/app/database";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json(tickets);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  console.log(body);
+
+  return NextResponse.json(body);
+}
