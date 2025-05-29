27f6a619ed9e1b783f47a34fa29e1a5c4704fb17
coispartan <hignacio_irri@hotmail.com>
2025-05-29 11:56:08 -0400

    Account Auth Implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -49,22 +49,14 @@ const questions = [
   },
 ];
 
-const test = async () => {
-  try {
-    return await api.users.getAll();
-  } catch (error) {
-    return handleError(error);
-  }
-};
-
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const users = await test();
+  const session = await auth();
 
-  console.log(users);
+  console.log("Session: ", session);
 
   const { query = "", filter = "" } = await searchParams;
 
