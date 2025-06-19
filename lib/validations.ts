6f00eb001874e302c22eede7c70315ae6017922a
coispartan <hignacio_irri@hotmail.com>
2025-06-19 11:00:23 -0400

    Edit and Get Server actions Implementation

--- lib/validations.ts
+++ lib/validations.ts
@@ -124,3 +124,11 @@ export const SignInWithOauthSchema = z.object({
     image: z.string().url("Invalid image URL"),
   }),
 });
+
+export const EditQuestionSchema = AskQuestionSchema.extend({
+  questionId: z.string().min(1, { message: "Question ID is required." }),
+});
+
+export const GetQuestionSchema = z.object({
+  questionId: z.string().min(1, { message: "Question ID is required." }),
+});
